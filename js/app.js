0a7bc12b60828c9c87b295eb45fc47eaeef1c6be
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-02-07 21:51:14 -0600

    feat: button for deleting and editing users added (without a function yet)

--- js/app.js
+++ js/app.js
@@ -116,13 +116,35 @@ function printUsers() {
             <span class="font-weight-bolder">Password: </span>${password}
         `;
 
+        const btnDelete=document.createElement('button');
+        btnDelete.classList.add('btn', 'btn-danger', 'mr-2');
+        btnDelete.innerHTML='Remove <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6"> <path fill-rule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" /> </svg>'
+
+        btnDelete.onclick=()=>deleteUser(id);
+
+        const btnEdit=document.createElement('button');
+        btnEdit.classList.add('btn', 'btn-info');
+        btnEdit.innerHTML='Edit <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6"> <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" /></svg>'
+
+        btnEdit.onclick=()=>editUser(user);
+
         divUser.appendChild(userNameParagraph);
         divUser.appendChild(emailParagraph);
         divUser.appendChild(phoneParagraph);
         divUser.appendChild(passwordParagraph);
+        divUser.appendChild(btnDelete);
+        divUser.appendChild(btnEdit);
 
         containerUsers.appendChild(divUser);
     });
+}
+
+function deleteUser(id) {
+
+}
+
+function editUser(user) {
+
 }
 
 function cleanHTML(spaceToClean) {
